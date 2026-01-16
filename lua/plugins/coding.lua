579081d8e1f8ad4dfd4f3da9791138afb3ec1d73
Linh Tran <linh.mtran168@live.com>
2026-01-16 10:56:27 +0700

    feat: use super tab in blink

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,1 +1,15 @@
-return {}
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "super-tab",
+    },
+    completion = {
+      list = {
+        preselect = function(_)
+          return not require("blink.cmp").snippet_active({ direction = 1 })
+        end,
+      },
+    },
+  },
+}
